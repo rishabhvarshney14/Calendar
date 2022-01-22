f6540d45f4b4f896686fbfd17b1ff754695c6c74
Rishabh Varshney <rishabhvarshney14@gmail.com>
2022-01-22 21:56:28 +0530

    Fixed starting index in Carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,17 +3,25 @@ import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import Card from './Tile/Card';
 
-const Carousel = ({data}) => {
+const Carousel = ({data, modalData}) => {
     const [items, setItems] = useState([])
+    const [index, setIndex] = useState(0)
+
     const responsive = {
         0: { items: 1 },
         568: { items: 2 },
-        1024: { items: 3 },
+        1024: { items: 4 },
     };
 
     useEffect(() => {
         const createItems = () => {
-            const tempItems = Object.values(data).map((item => <Card data={item} />))
+            const tempItems = Object.values(data).map(((item, idx) => {
+                if (modalData && item.id === modalData.id) {
+                    setIndex(idx);
+                }
+                console.log(modalData)
+                return <Card data={item} />
+            }))
             setItems(tempItems)
         }
 
@@ -23,6 +31,7 @@ const Carousel = ({data}) => {
     return (
         <div className='pt-2'>
         {items.length > 0 && <AliceCarousel
+                activeIndex={index}
                 infinite={true}
                 mouseTracking
                 items={items}
